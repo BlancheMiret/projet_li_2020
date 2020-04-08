cc49bec75bfaff92fcddbc65681cf6a2b12dd4b9
MIRET blanche <blanchemiret@gmail.com>
2020-04-08 11:24:30 +0200

    Update wiki_copy.py

--- src/wiki_copy.py
+++ src/wiki_copy.py
@@ -16,8 +16,6 @@ wiki = wikipediaapi.Wikipedia(
 	language = 'fr',
 	extract_format = wikipediaapi.ExtractFormat.WIKI)
 
-print(1)
-
 for i in range(1, len(sys.argv)) :
 
 	#livre
@@ -26,7 +24,6 @@ for i in range(1, len(sys.argv)) :
 
 	#retrieve the page
 	types = [' (livre)', ' (roman)', ' (bande dessinée)', '' ]
-
 	page = ''
 	for t in types :
 		page =  wiki.page(sys.argv[i] + t)
@@ -46,7 +43,7 @@ for i in range(1, len(sys.argv)) :
 
 	#write the file
 	with open(name, 'w+') as fd :
-		fd.write(sys.argv[1] + "\n\n")
+		fd.write(sys.argv[i] + "\n\n")
 		for line in page.text :
 			fd.write(line)
 
