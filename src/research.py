d735e42f94c1cc68ec1b5d73d9232a563b02bcbd
MIRET blanche <blanchemiret@gmail.com>
2020-04-22 11:33:01 +0200

    Create research.py

---
+++ src/research.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Étapes :
+# - Récupérer mots clés de la recherche passée en ligne de commande
+# - Créer matrice des coordonnées (documents + requête / termes)
+# - Créer tableau des distances (requête / documents)
+# - Choisir quels documents correspondent à la recherche et afficher leur nom
